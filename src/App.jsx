6e34b4d86fae2daf9b36aabe2cb794845ecc9963
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2023-06-16 21:23:30 -0300

    Clase 19 - Context pt 1

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,21 @@ import { routes } from './utils/routes'
 import Recipes from './Pages/Recipes'
 import Detail from './Pages/Detail'
 import Layout from './Layout/Layout'
+import ParentComponent from './Clase 18/ParentComponent'
+import LoboEstepario from './Clase 18/LoboEstepario'
+import { useState } from 'react'
+import ChildComponent from './Clase 18/ChildComponent'
 
 function App() {
 
+  const [salario, setSalario] = useState(150000)
 
   return (
     <>
-      <Routes>
+      <ParentComponent salario={salario}/>
+      <LoboEstepario salario={salario}/>
+
+      {/* <Routes>
         <Route path='/' element={<Layout/>}>
           <Route path={routes.home} element={<Home/>}/>
           <Route path={routes.contact} element={<Contact/>}/> 
@@ -24,7 +32,7 @@ function App() {
           <Route path={routes.detail} element={<Detail/>}/>
           <Route path={routes.pageNotFound} element={<PageNotFound/>}/>
         </Route>
-      </Routes>
+      </Routes> */}
     </>
   )
 }
